2fb5d7f407527c350332a0daad325be1a3dd5586
hgokhru101 <hgokhru101@gmail.com>
2020-02-29 12:20:31 +0530

    commit with frontend modification

---
+++ doctor/blank.php
@@ -0,0 +1,18 @@
+<?php include('../includes/header.php');?>
+
+    <?php include('sidebar.php');?>
+
+        <?php include('../includes/topbar.php');?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      <?php include('../includes/footer.php');
+            include('../includes/scripts.php');
+      ?>
